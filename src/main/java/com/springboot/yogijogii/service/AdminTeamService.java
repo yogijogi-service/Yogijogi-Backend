e1affc50119a55bc758fe2b59c810c2a6c96096d
Kim-Yu-Seong <107562291+kimys5278@users.noreply.github.com>
2024-11-18 23:06:23 +0900

    관리자 포메이션 등록 페이지-선수 포지션으로 선수 조회API

--- src/main/java/com/springboot/yogijogii/service/AdminTeamService.java
+++ src/main/java/com/springboot/yogijogii/service/AdminTeamService.java
@@ -5,9 +5,10 @@ import com.springboot.yogijogii.data.dto.teamStrategy.TeamMemberByPositionDto;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
+import java.util.Optional;
 
 public interface AdminTeamService {
     ResultDto updateSubManagerRole(HttpServletRequest servletRequest, Long teamMemberId, boolean grant);
-
+    List<TeamMemberByPositionDto> getTeamMemberByPosition(Long teamId,String position,HttpServletRequest servletRequest);
 }
 
