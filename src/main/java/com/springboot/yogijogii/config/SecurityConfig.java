511f4d5f8f9f16c8f5eaa4580cb5a5af2af77264
Kim-Yu-Seong <107562291+kimys5278@users.noreply.github.com>
2024-11-21 23:01:53 +0900

    getTeam 수정

--- src/main/java/com/springboot/yogijogii/config/SecurityConfig.java
+++ src/main/java/com/springboot/yogijogii/config/SecurityConfig.java
@@ -35,7 +35,9 @@ public class SecurityConfig extends  WebSecurityConfigurerAdapter{
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.httpBasic().disable() // REST API는 UI를 사용하지 않으므로 기본설정을 비활성화
+
                 .csrf().disable() // REST API는 csrf 보안이 필요 없으므로 비활성화
+
                 .sessionManagement()
                 .sessionCreationPolicy(
                         SessionCreationPolicy.STATELESS) // JWT Token 인증방식으로 세션은 필요 없으므로 비활성화
@@ -44,7 +46,7 @@ public class SecurityConfig extends  WebSecurityConfigurerAdapter{
                 .authorizeRequests() // 리퀘스트에 대한 사용권한 체크
                 .antMatchers("/api/sign/**",
                         "/sign/exception","/main-api/**","/auth/**").permitAll() // 가입 및 로그인 주소는 허용
-                .antMatchers("/api/team/**","/api/search/**").authenticated()
+                .antMatchers("/api/team/**","api/search/**").authenticated()
                 .antMatchers("/api/announcement/manager/**").hasRole("MANAGER")
                 .antMatchers("/test/**").permitAll()
                 .antMatchers("**exception**").permitAll()
